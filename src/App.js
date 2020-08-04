d9e376ae10785e6a5b8d23a0b6c53327ee986432
Mananchery Panneerselvam <mppk2009@gmail.com>
2020-08-03 20:09:13 -0600

    optimized the background style to a variable in js code

--- src/App.js
+++ src/App.js
@@ -8,20 +8,31 @@ const App = () => {
 
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
-
+    
     const search = async(e) => {
         if (e.key === 'Enter'){
             const data = await fetchWeather(query);
-            console.log(data);
             setWeather(data);
             setQuery('');
         }
+
     }
 
+    let styles  = {
+        background: 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.418) ), url(https://www.bluefinllc.com/wp-content/uploads/2019/08/AdobeStock_120884346_sm-e1566503899644.jpg )',
+        /* background-image: url('https://www.bluefinllc.com/wp-content/uploads/2019/08/AdobeStock_120884346_sm-e1566503899644.jpg    '); */
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    }
+
+
+
     return (
-        <div className = 'main-container'>
+        <div className = 'main-container' style = {styles}>
+            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300&display=swap" rel="stylesheet"></link>
+            <h1 class = "heading"> Weather App </h1>
             <input
-                type = 'text'
+                type = 'search'
                 className = 'search'
                 placeholder = 'Search'
                 value = {query}
@@ -41,6 +52,7 @@ const App = () => {
                     <div className = 'info'>
                         <img className= 'city-icon' src = {`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}></img>
                         <p> {weather.weather[0].description}</p>
+                        {console.log(weather.weather[0].description.includes('clear'))}
                     </div>
                     <div className = 'city-temp-max-min'>
                         Max Temperature:{Math.round(weather.main.temp_max)}
@@ -52,6 +64,7 @@ const App = () => {
  
                 </div>
             )}
+
         </div>
     )
 };
