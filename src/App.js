3ca337b90a8976070ec3ded288f3750f91b09382
Mananchery Panneerselvam <mppk2009@gmail.com>
2020-08-03 20:20:37 -0600

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ const App = () => {
 
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
-    
+    const [styles, setStyles] = useState({
+        background: 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.418) ), url(https://www.bluefinllc.com/wp-content/uploads/2019/08/AdobeStock_120884346_sm-e1566503899644.jpg )',
+        /* background-image: url('https://www.bluefinllc.com/wp-content/uploads/2019/08/AdobeStock_120884346_sm-e1566503899644.jpg    '); */
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    })
     const search = async(e) => {
         if (e.key === 'Enter'){
             const data = await fetchWeather(query);
@@ -18,12 +23,12 @@ const App = () => {
 
     }
 
-    let styles  = {
-        background: 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.418) ), url(https://www.bluefinllc.com/wp-content/uploads/2019/08/AdobeStock_120884346_sm-e1566503899644.jpg )',
-        /* background-image: url('https://www.bluefinllc.com/wp-content/uploads/2019/08/AdobeStock_120884346_sm-e1566503899644.jpg    '); */
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-    }
+    // let styles  = {
+    //     background: 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.418) ), url(https://www.bluefinllc.com/wp-content/uploads/2019/08/AdobeStock_120884346_sm-e1566503899644.jpg )',
+    //     /* background-image: url('https://www.bluefinllc.com/wp-content/uploads/2019/08/AdobeStock_120884346_sm-e1566503899644.jpg    '); */
+    //     backgroundSize: 'cover',
+    //     backgroundPosition: 'center'
+    // }
 
 
 
@@ -52,7 +57,6 @@ const App = () => {
                     <div className = 'info'>
                         <img className= 'city-icon' src = {`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}></img>
                         <p> {weather.weather[0].description}</p>
-                        {console.log(weather.weather[0].description.includes('clear'))}
                     </div>
                     <div className = 'city-temp-max-min'>
                         Max Temperature:{Math.round(weather.main.temp_max)}
